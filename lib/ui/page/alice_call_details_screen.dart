7cae2bfd830af17f565aae3151bdea8258fb9847
Heryan <febriansyah.online@gmail.com>
2024-03-20 21:48:22 +0700

    disable share function

--- lib/ui/page/alice_call_details_screen.dart
+++ lib/ui/page/alice_call_details_screen.dart
@@ -8,7 +8,7 @@ import 'package:flutter_alice/ui/widget/alice_call_overview_widget.dart';
 import 'package:flutter_alice/ui/widget/alice_call_request_widget.dart';
 import 'package:flutter_alice/ui/widget/alice_call_response_editor_widget.dart';
 import 'package:flutter_alice/ui/widget/alice_call_response_widget.dart';
-import 'package:share_plus/share_plus.dart';
+// import 'package:share_plus/share_plus.dart';
 
 class AliceCallDetailsScreen extends StatefulWidget {
   final AliceHttpCall call;
@@ -61,15 +61,15 @@ class _AliceCallDetailsScreenState extends State<AliceCallDetailsScreen>
     return DefaultTabController(
       length: 5,
       child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: AliceConstants.lightRed,
-          key: Key('share_key'),
-          onPressed: () async {
-            Share.share(await _getSharableResponseString(),
-                subject: 'Request Details');
-          },
-          child: Icon(Icons.share),
-        ),
+        // floatingActionButton: FloatingActionButton(
+        //   backgroundColor: AliceConstants.lightRed,
+        //   key: Key('share_key'),
+        //   onPressed: () async {
+        //     Share.share(await _getSharableResponseString(),
+        //         subject: 'Request Details');
+        //   },
+        //   child: Icon(Icons.share),
+        // ),
         appBar: AppBar(
           bottom: TabBar(
             indicatorColor: AliceConstants.lightRed,
